8d32a622416f84472f4ddcfb7bc726cc928c6909
alexellipse, seinsdieum <alexellipse@yandex.by>
2025-04-14 14:15:14 +0300

    refactor: rewrite in async/await

--- index.js
+++ index.js
@@ -6,44 +6,44 @@ const fs = require('./modules/file-system')
 const cache = require('./modules/cache')
 
 const getArgs = () => {
-    let args = process.argv.slice(2)
-    let parsed = {}
-    for (let i of args) {
-        if (i.includes('--')) {
-            let sliced = i.slice(2)
-            if (!sliced) {
-                logger.error('argument not defined')
-                continue
-            }
-            const [key, ...rest] = sliced.split('=')
-            parsed[key] = rest.length > 0 ? rest.join('=') : true
-        }
+  let args = process.argv.slice(2)
+  let parsed = {}
+  for (let i of args) {
+    if (i.includes('--')) {
+      let sliced = i.slice(2)
+      if (!sliced) {
+        logger.error('argument not defined')
+        continue
+      }
+      const [key, ...rest] = sliced.split('=')
+      parsed[key] = rest.length > 0 ? rest.join('=') : true
     }
-    return parsed
+  }
+  return parsed
 }
 let args = getArgs(process.argv)
 
-if (args.searchTerm) {
-    cache.get(args.searchTerm).then(res => {
-        if (!res) {
-            api.getRandomJokeFromSearch(args.searchTerm, res => {
-                logger.joke(res)
-                fs.writeJokes([res])
-                cache.add(args.searchTerm, res)
-            })
-        } else {
-            logger.joke(res)
-            fs.writeJokes([res])
-        }
-    })
-} else if (args.leaderboard) {
-    fs.readJokes().then(res => {
-        res = leaderboard.getLeaderboard(res)
-        logger.leaderboard(res)
-    })
-} else {
+async function run() {
+  if (args.searchTerm) {
+    let res
+    if (!(res = await cache.get(args.searchTerm))) {
+      api.getRandomJokeFromSearch(args.searchTerm, res => {
+        logger.joke(res)
+        fs.writeJokes([res])
+        cache.add(args.searchTerm, res)
+      })
+    } else {
+      logger.joke(res)
+      fs.writeJokes([res])
+    }
+  } else if (args.leaderboard) {
+    const res = await fs.readJokes()
+    logger.leaderboard(leaderboard.getLeaderboard(res))
+  } else {
     api.fetchRandomJoke(d => {
-        logger.joke(d)
-        fs.writeJokes([d])
+      logger.joke(d)
+      fs.writeJokes([d])
     })
+  }
 }
+run()
