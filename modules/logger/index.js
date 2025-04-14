9fecf41a120c8a87cfa788a8592f591e0cbced5a
alexellipse, seinsdieum <alexellipse@yandex.by>
2025-04-14 13:58:50 +0300

    refactor: fix logger code tab

--- modules/logger/index.js
+++ modules/logger/index.js
@@ -1,34 +1,34 @@
 const colors = {
-    reset: '\x1b[0m',
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m',
-    blue: '\x1b[34m'
+  reset: '\x1b[0m',
+  red: '\x1b[31m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[34m'
 }
 
 function error(message) {
-    console.log(
-        colors.red,
-        'error:',
-        message || 'message not defined',
-        colors.reset
-    )
+  console.log(
+    colors.red,
+    'error:',
+    message || 'message not defined',
+    colors.reset
+  )
 }
 
 function joke(joke) {
-    if (!joke) {
-        console.log(colors.red, 'no jokes were found for you...', colors.reset)
-        return
-    }
-    console.log(colors.blue, joke.joke, colors.reset)
+  if (!joke) {
+    console.log(colors.red, 'no jokes were found for you...', colors.reset)
+    return
+  }
+  console.log(colors.blue, joke.joke, colors.reset)
 }
 
 function leaderboard(jokes) {
-    if (!jokes?.length) {
-        console.log(colors.red, 'no jokes for leaderboard...', colors.reset)
-        return
-    }
-    console.table(jokes)
+  if (!jokes?.length) {
+    console.log(colors.red, 'no jokes for leaderboard...', colors.reset)
+    return
+  }
+  console.table(jokes)
 }
 
 module.exports = { joke, leaderboard, error }
